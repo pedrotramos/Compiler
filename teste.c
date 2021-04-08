9abc090b747e0231ee93a40d8c507b2bceeed231
Pedro Ramos <pedrotramos97@gmail.com>
2021-04-08 15:09:50 -0300

    Melhoria arquivo de teste

--- teste.c
+++ teste.c
@@ -1,5 +1,5 @@
 a = 2;
-b = 8;
-c = a;
-delta = (b * b - 4 * a * c); /* Delta da Fórmula de Bhaskara*/
+b = (3 - 2) * ((4 + 12) / 2); /* 8 */
+c = a + 1;                    /* 3 */
+delta = (b * b - 4 * a * c);  /* Delta da Fórmula de Bhaskara */
 println(delta);
