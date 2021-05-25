cb1403053dbe066be83e6691f3898116290c654c
Pedro Ramos <pedrotramos97@gmail.com>
2021-05-24 22:19:12 -0300

    Código usado para passar na v0.1

--- README.md
+++ README.md
@@ -6,43 +6,4 @@
 
 ### Funcionamento do compilador:
 
-Esse compilador funciona somente para argumentos de entrada que buscam calcular operações aritiméticas, booleanas e relativas, atribuições a variáveis e comandos de *println*, *readln*, *while* e *if-else*. O programa deve ser executado da seguinte forma:
-
-~~~
-python main.py "arquivo.c"
-~~~
-
-Sendo ```arquivo.c``` o arquivo que contém a equação a ser resolvida. Assim, o programa exibirá na tela como saída a resolução do programa ```arquivo.c```.
-
-### Diagrama Sintático:
-
-<img src="Assets/DiagramaSintatico.png"/>
-
-### Notação EBNF:
-
-<img src="Assets/EBNF.png"/>
-
-### EBNF Atual:
-
-~~~
-BLOCK = "{", { COMMAND }, "}" ;
-COMMAND = ((λ | ASSIGNMENT | DECLARATION | PRINT), ";" | LOOP | IF-ELSE) ;
-LOOP = "while", "(", OR_EXPRESSION, ")", BLOCK ;
-IF-ELSE = "if", "(", OR_EXPRESSION, ")", BLOCK, ["else", BLOCK] ;
-DECLARATION = TYPE, IDENTIFIER, ["=", (OR_EXPRESSION | READ)] ;
-ASSIGNMENT = IDENTIFIER, "=", (OR_EXPRESSION | READ) ;
-READ = "readln", "(", ")" ;
-PRINT = "println", "(", OR_EXPRESSION, ")" ;
-OR_EXPRESSION = AND_EXPRESSION, { "||", AND_EXPRESSION } ;
-AND_EXPRESSION = EQ_EXPRESSION, { "&&", EQ_EXPRESSION } ;
-EQ_EXPRESSION = REL_EXPRESSION, { "==", REL_EXPRESSION } ;
-REL_EXPRESSION = EXPRESSION, { (">" | "<"), EXPRESSION } ;
-EXPRESSION = TERM, { ("+" | "-"), TERM } ;
-TERM = FACTOR, { ("*" | "/"), FACTOR } ;
-FACTOR = ((("+" | "-"), FACTOR) | NUMBER | "(", EXPRESSION, ")" | IDENTIFIER | STRING);
-IDENTIFIER = LETTER, { LETTER | DIGIT | "_" } ;
-NUMBER = DIGIT, { DIGIT } ;
-LETTER = ( a | ... | z | A | ... | Z ) ;
-STRING = """, LETTER, { LETTER }, """ ;
-DIGIT = ( 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 0 ) ;
-~~~
+python main.py "1+1"
--- main.py
+++ main.py
@@ -1,21 +1,57 @@
 import sys
-import argparse
-from Pacotes.preprocessor import PreProcessor
-from Pacotes.parser import Parser
+
+
+class Calculator:
+    def __init__(self):
+        self.txt = None
+        self.result = 0
+        self.support = ""
+        self.valid = "1234567890+- \t"
+        self.list_sign = []
+        self.list_num = []
+        self.error = False
+
+    def parse(self, arg):
+        self.txt = arg.replace(" ", "")
+        for i in range(len(self.txt)):
+            if self.txt[i] not in self.valid:
+                print("Argumento Inválido. Há caracteres inválidos no argumento.")
+                self.error = True
+                break
+            if i == 0:
+                if self.txt[i] in "-+":
+                    print("Argumento Inválido. Há problemas com os sinais.")
+                    self.error = True
+                    break
+                else:
+                    self.list_sign.append(1)
+                    self.support += self.txt[i]
+            else:
+                if self.txt[i] in "+-":
+                    self.support += " "
+                    if self.txt[i] == "+":
+                        self.list_sign.append(1)
+                    else:
+                        self.list_sign.append(-1)
+                else:
+                    self.support += self.txt[i]
+
+        self.list_num = self.support.split()
+
+        if len(self.list_sign) != len(self.list_num):
+            print("Argumento Inválido. Há problemas com os sinais.")
+            self.error = True
+
+    def calculate(self):
+        if not self.error:
+            for num, sign in zip(self.list_num, self.list_sign):
+                self.result += sign * int(num)
+            print(calc.result)
 
 
 if __name__ == "__main__":
 
-    if len(sys.argv) > 1:
-        if sys.argv[1].endswith(".c"):
-            with open(sys.argv[1], "r") as f:
-                arg = f.read()
-            txt = PreProcessor().filter(src=arg)
-            result = Parser().run(txt)
-            print(result)
-        else:
-            raise argparse.ArgumentError(
-                "O argumento do programa deve ser um arquivo C"
-            )
-    else:
-        raise argparse.ArgumentError("O programa precisa de um argumento para rodar")
+    arg = sys.argv[1]
+    calc = Calculator()
+    calc.parse(arg)
+    calc.calculate()
